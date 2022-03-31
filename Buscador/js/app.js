b4bae2f6c0931698641122dd72f4557b7f3575af
Martin Castro <martin.castro1575@gmail.com>
2022-03-31 08:12:01 -0300

    Se agrega filtrado para marca y anio de auto

--- Buscador/js/app.js
+++ Buscador/js/app.js
@@ -1,23 +1,72 @@
 //var
-const resultados = document.querySelector('#resultado')
+const marca = document.querySelector('#marca')
 const year = document.querySelector('#year')
+const minPrice = document.querySelector('#minimo')
+const maxPrice = document.querySelector('#maximo')
+const puertas = document.querySelector('#puertas')
+const transmision = document.querySelector('#transmision')
+const color = document.querySelector('#color')
+const resultados = document.querySelector('#resultado')
 
 const maxYear = new Date().getFullYear()
 const minYear = maxYear - 10
 
 
-
+//Genero un objeto para la busqueda segun sea la eleccion del usuario
+const datosBusqueda = {
+    marca:'',
+    year:'',
+    minPrice:'',
+    maxPrice:'',
+    puertas:'',
+    transmision:'',
+    color:'',
+}
 
 
 //events
 document.addEventListener('DOMContentLoaded', ()=>{
-    mostrarAutos()
+    mostrarAutos(autos)
 
     llenarSelect()
 })
 
+//listener para los select de busqueda
+marca.addEventListener('change',e=>{
+    datosBusqueda.marca = e.target.value
+
+    filtrarAuto()
+})
+
+year.addEventListener('change',e=>{
+    datosBusqueda.year = parseInt(e.target.value)
+    filtrarAuto()
+})
+
+minPrice.addEventListener('change',e=>{
+    datosBusqueda.minPrice = parseInt(e.target.value)
+})
+
+maxPrice.addEventListener('change',e=>{
+    datosBusqueda.maxPrice = parseInt(e.target.value)
+})
+
+puertas.addEventListener('change',e=>{
+    datosBusqueda.puertas = parseInt(e.target.value)
+})
+
+transmision.addEventListener('change',e=>{
+    datosBusqueda.transmision = e.target.value
+})
+
+color.addEventListener('change',e=>{
+    datosBusqueda.color = e.target.value
+    
+})
+
 //functions
-function mostrarAutos(){
+function mostrarAutos(autos){
+    limpiarHTML() //Actualizando html
     autos.forEach(auto => {
         const autoHTML = document.createElement('p')
 
@@ -31,6 +80,13 @@ function mostrarAutos(){
     })
 }
 
+//Limpiar HTML para la busqueda
+function limpiarHTML(){
+    while (resultados.firstChild) {
+        resultados.removeChild(resultados.firstChild)
+    }
+}
+
 function llenarSelect(){
     for (let i = maxYear; i >= minYear; i--) {
         const opcion = document.createElement('option')
@@ -38,4 +94,29 @@ function llenarSelect(){
         opcion.textContent = i
         year.appendChild(opcion)
     }
+}
+
+function filtrarAuto(){
+     const resultado = autos.filter(filtrarMarca).filter(filtrarYear)
+
+     mostrarAutos(resultado)
+    // console.log(resultado)
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda
+
+    if (marca){
+        return auto.marca === marca 
+    }
+    return auto
+}
+
+function filtrarYear(auto){
+    const {year} = datosBusqueda
+
+    if (year){
+        return auto.year === year
+    }
+    return auto
 }
